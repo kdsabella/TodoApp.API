438ec0914a487b25c4d360121908d99b9c6335f4
Ken Sabella <Kensabella04@gmail.com>
2024-02-16 10:17:52 +0800

    add context db

---
+++ TodoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Todo_API.Models;
+
+namespace Todo_API.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public  TodoDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
